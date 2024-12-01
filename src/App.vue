f2b525425dd8ed54805e50040f2c9166c57306eb
Lionel <bachmannlionel@gmail.com>
2024-12-01 19:35:31 +0100

    ajout page WeekRoomView TEST KO (problemes esLint)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/weekRoomView">weekRoomView</router-link>
   </nav>
   <router-view />
 </template>
--- src/views/WeekRoomView.vue
+++ src/views/WeekRoomView.vue
@@ -1,11 +1,11 @@
 <template>
-
+    <div class="WeekRoomView">
+        <h1>Agenda</h1>
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style></style>
