f528fb83ce4523976c011838dea6bd4dbae31bff
Lionel <bachmannlionel@gmail.com>
2024-12-01 22:24:39 +0100

    ajout de l'action de recupération des salles ds le soomStore + changement api.js en api.ts

---
+++ src/stores/roomStore.ts
@@ -0,0 +1,26 @@
+import { defineStore } from "pinia";
+import { Room } from "@/types/room";
+import apiClient from "@/api/api";
+
+export const useRoomStore = defineStore("room", {
+  state: () => ({
+    rooms: [] as Room[],
+    loading: false,
+    error: null as string | null,
+  }),
+
+  actions: {
+    async fetchRooms() {
+      this.loading = true;
+      try {
+        const response = await apiClient.get<Room[]>("/rooms");
+        this.rooms = response.data;
+      } catch (error) {
+        this.error =
+          error instanceof Error ? error.message : "Une erreur est survenue";
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+});
