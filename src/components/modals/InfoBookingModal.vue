a9c653b3b8d673307cc2603b4344e92b61b20f4a
Lionel <bachmannlionel@gmail.com>
2024-12-09 22:18:36 +0100

    ajout style  pour modal d'info ( centré, fond grisé)

--- src/components/modals/InfoBookingModal.vue
+++ src/components/modals/InfoBookingModal.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="modal">
-    <h3>Détails de la réservation</h3>
-    <p>Titre : {{ booking.bookingTitle }}</p>
-    <p>Organisateur : {{ booking.userEmail }}</p>
+  <div class="modal-wrapper">
+    <div class="modal">
+      <h3>Détails de la réservation</h3>
+      <p>Titre : {{ booking.bookingTitle }}</p>
+      <p>Organisateur : {{ booking.userEmail }}</p>
+    </div>
+
+    <div class="modal-overlay"></div>
   </div>
 </template>
 
@@ -14,17 +18,37 @@ export default {
       required: true,
     },
   },
-  methods: {},
 };
 </script>
 
 <style scoped>
+.modal-wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
 .modal {
   background: white;
   border: 1px solid #ddd;
   padding: 20px;
   max-width: 400px;
-  margin: 0 auto;
+  width: 90%;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  z-index: 1000;
 }
 </style>
