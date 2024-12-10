ed531bfca459626d379fa79683cdd981cfd1b8a5
Lionel <bachmannlionel@gmail.com>
2024-12-11 00:58:30 +0100

    ajout vuetify au main

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App);
 const pinia = createPinia();
 
-app.use(router).use(pinia).mount("#app");
+app.use(router).use(pinia).use(vuetify).mount("#app");
